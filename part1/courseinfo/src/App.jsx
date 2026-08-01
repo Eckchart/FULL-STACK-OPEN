66c9dd7ba145796ccc9f36d89d24f04cd48d827a
Eckchart <thesynner03@gmail.com>
2026-08-01 18:28:11 +0300

    Add Header, Content, Total components

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <p>{props.parts[0]} {props.nrExercises[0]}</p>
+            <p>{props.parts[1]} {props.nrExercises[1]}</p>
+            <p>{props.parts[2]} {props.nrExercises[2]}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>Number of exercises {props.totalNrExercises}</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+    return (
+        <div>
+            <Header course={course} />
+            <Content parts={[part1, part2, part3]} nrExercises={[exercises1, exercises2, exercises3]} />
+            <Total totalNrExercises={exercises1 + exercises2 + exercises3} />
+        </div>
+    )
+}
+
+export default App
