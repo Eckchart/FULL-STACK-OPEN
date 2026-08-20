f5a01ccaa1cdb4147811d3c9edaff0e2b2dfe51f
Eckchart <thesynner03@gmail.com>
2026-08-20 16:56:03 +0300

    Initialize unicafe project and add all/average/positive statistics

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => setGood(good + 1)
+
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+
+    const handleBadClick = () => setBad(bad + 1)
+
+    const totalFeedback = good + neutral + bad
+    const avgScore = totalFeedback ? (good - bad) / totalFeedback : "undefined"
+    const positiveFeedbackPercentage = totalFeedback ? good / totalFeedback + "%" : "undefined"
+
+    return (
+        <div>
+            <h2>give feedback</h2>
+            <button onClick={handleGoodClick}>good</button>
+            <button onClick={handleNeutralClick}>neutral</button>
+            <button onClick={handleBadClick}>bad</button>
+
+            <h2>statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {totalFeedback}</p>
+            <p>average {avgScore}</p>
+            <p>positive {positiveFeedbackPercentage}</p>
+        </div>
+    )
+}
+
+export default App
