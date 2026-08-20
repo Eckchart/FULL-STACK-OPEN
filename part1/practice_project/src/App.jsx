1aab8fb4d9785c53df2aa52cca9f9738614f172a
Eckchart <thesynner03@gmail.com>
2026-08-20 16:26:21 +0300

    Finish part 1 theory

--- part1/practice_project/src/App.jsx
+++ part1/practice_project/src/App.jsx
@@ -1,26 +1,57 @@
-const Hello = (props) => {
-    console.log(props)
+import { useState } from 'react'
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            button press history: {props.allClicks.join(' ')}
         </div>
     )
 }
 
+const Button = (props) => (
+    <button onClick={props.onClick}>
+        {props.text}
+    </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
 const App = () => {
-    const friends = [
-        { name: 'Peter', age: 4 },
-        { name: 'Maya', age: 10 },
-    ]
-    const friends2 = ['peter', 'maya']
-    
-    return (
-        <div>
-            <p>{friends[0].name} {friends[0].age}</p>
-            <p>{friends[1].name} {friends[1].age}</p>
-            <p>{friends2}</p>
-        </div>
-    )
+  const [value, setValue] = useState(10)
+
+  const handleClick = () => {
+    console.log('clicked the button')
+    setValue(0)
+  }
+  
+  const hello = (who) => () => console.log("hello world", who)
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  const setToValue2 = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+        <Display value={value} />
+        <Button onClick={() => setToValue2(1000)} text="thousand" />
+        <Button onClick={() => setToValue2(0)} text="reset" />
+        <Button onClick={() => setToValue2(value + 1)} text="increment" />
+    </div>
+  )
 }
 
 export default App
