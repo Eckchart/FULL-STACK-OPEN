711d17110955f1fc260bca5597819b2f363fb0cc
Eckchart <thesynner03@gmail.com>
2026-08-24 17:42:40 +0300

    Add search field to filter the list of people by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '0234567891' }
-  ]) 
+    { name: 'Arto Hellas', number: '0234567891' },
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'} 
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -31,10 +35,25 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value)
+  }
+
+  const personsToShow = filterText == ''
+    ? persons
+    : persons.filter(person => (
+        person.name.toLowerCase().includes(filterText.toLowerCase())
+      ))
   
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filterText} onChange={handleFilterTextChange}/>
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -48,9 +67,11 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map(person =>
-        <p key={person.name}>{person.name} {person.number}</p>
-      )}
+      <div>
+        {personsToShow.map(person =>
+          <p key={person.name}>{person.name} {person.number}</p>
+        )}
+      </div>
     </div>
   )
 }
