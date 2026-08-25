7b2408cea436be09d1b2c8d2a8a6e27414cbb7bf
Eckchart <thesynner03@gmail.com>
2026-08-25 21:57:50 +0300

    Finish Getting data from server

--- part2/practice_project/src/App.jsx
+++ part2/practice_project/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Note from "./components/Note"
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
-  const [showAll, setShowAll] = useState(false)
-  
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3003/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes');
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -26,8 +39,6 @@ const App = (props) => {
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
-  // console.log(notesToShow);
-    
   
   return (
     <div>
