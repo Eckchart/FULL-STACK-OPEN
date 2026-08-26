d39f40a92ee8ab19ef88bbf75ea3407d005f3cd1
Eckchart <thesynner03@gmail.com>
2026-08-26 22:41:12 +0300

    Save persons added to the phonebook to a backend server in a separate module

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,12 +11,12 @@ const App = () => {
   const [filterText, setFilterText] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3002/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
-  }, []);
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,10 +30,13 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
-    setPersons(persons.concat(newPersonObject))
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(newPersonObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   const handleNameChange = (event) => {
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+// PERSONS IN THE BACKEND = NUMBERS IN THE HTML PAGE BASICALLY,,,
+const baseUrl = 'http://localhost:3003/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
+}
+
+export default { getAll, create, update }
