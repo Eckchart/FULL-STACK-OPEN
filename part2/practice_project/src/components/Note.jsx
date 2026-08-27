741c5cb3f90e84bb6b7ffe17072734c738033e2c
Eckchart <thesynner03@gmail.com>
2026-08-27 16:11:46 +0300

    Finish Adding styles to React app

--- part2/practice_project/src/components/Note.jsx
+++ part2/practice_project/src/components/Note.jsx
@@ -2,8 +2,9 @@ const Note = ({ note, toggleImportance }) => {
   const label = note.important ? 'make not important' : 'make important'
   
   return (
-    <li>
+    <li className="note">
       {note.content}
+      &nbsp;
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
