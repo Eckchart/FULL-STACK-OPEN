bcb0cdd9b78deab49762335bbbec05aca5c45c88
Eckchart <thesynner03@gmail.com>
2026-09-01 18:08:13 +0300

    Finish Deploying app to internet

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,10 +1,6 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174']
-}))
 app.use(express.static('dist'))
 
 
