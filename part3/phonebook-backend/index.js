2ea4e4ab96708ac66a07937c3b9cdddefb9b6af0
Eckchart <thesynner03@gmail.com>
2026-09-02 12:48:46 +0300

    Set PORT to use Render's environment variable or default to 3003

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -103,7 +103,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
