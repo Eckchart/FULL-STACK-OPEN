b490c63faa5620bf3b934f1bff7233027f687480
Eckchart <thesynner03@gmail.com>
2026-09-05 15:16:44 +0300

    Add notes backend and frontend from part3 to part4

---
+++ part4/notes-backend/index.js
@@ -0,0 +1,113 @@
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
+
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
+
+
+//////// MY MIDDLEWARE ///////////
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+//////////////////////////////////
+
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note
+    .findById(id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
+  Note
+    .findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  const id = request.params.id
+
+  Note
+    .findById(id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+      note.content = content
+      note.important = important
+      return note.save().then(updatedNote => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+// has to be at the end of the routes (ORDER MATTERS)
+app.use(unknownEndpoint)
+
+// this has to be the last loaded middleware (EVEN AFTER 'unknownEndpoint'),
+// also all the routes should be registered before this!
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
