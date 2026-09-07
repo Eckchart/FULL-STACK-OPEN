12b3a913465fd91d5b228292b04627cc23d09aa8
Eckchart <thesynner03@gmail.com>
2026-09-07 12:27:36 +0300

    Add tests for total likes

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,7 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const getTotalLikes = (blogsList) => {
+  return blogsList.reduce((acc, curBlog) => acc + curBlog.likes, 0)
+}
+
 
 module.exports = {
   dummy,
+  getTotalLikes
 }
