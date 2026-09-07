5917dd372be1a4329ca1e2b6679f322ba26fc216
Eckchart <thesynner03@gmail.com>
2026-09-07 13:34:25 +0300

    Add tests for favorite blog

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -6,8 +6,19 @@ const totalLikes = (blogsList) => {
   return blogsList.reduce((acc, curBlog) => acc + curBlog.likes, 0)
 }
 
+const favoriteBlog = (blogsList) => {
+  if (blogsList.length === 0) {
+    return null
+  }
+  const maxLikes = blogsList.reduce((maxSoFar, curBlog) =>
+    Math.max(maxSoFar, curBlog.likes), -Infinity
+  )
+  return blogsList.find(blog => blog.likes === maxLikes)
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
